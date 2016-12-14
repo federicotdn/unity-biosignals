9b989e4546461309c60fea3785887cf3390a27ad
Fede <federicotedin@gmail.com>
2016-12-14 11:34:05 -0300

    added python script

---
+++ Python/osc.py
@@ -0,0 +1,41 @@
+"""Small example OSC server
+
+This program listens to several addresses, and prints some information about
+received packets.
+"""
+import argparse
+import math
+
+from pythonosc import dispatcher
+from pythonosc import osc_server
+
+def print_volume_handler(unused_addr, args, volume):
+  print("[{0}] ~ {1}".format(args[0], volume))
+
+def print_compute_handler(unused_addr, args, volume):
+  try:
+    print("[{0}] ~ {1}".format(args[0], args[1](volume)))
+  except ValueError: pass
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--ip",
+      default="0.0.0.0", help="The ip to listen on")
+  parser.add_argument("--port",
+      type=int, default=5005, help="The port to listen on")
+  args = parser.parse_args()
+
+  dispatcher = dispatcher.Dispatcher()
+  dispatcher.map("/muse/elements/blink", print)
+
+#   dispatcher.map("/muse/elements/jaw_clench", print)
+#   dispatcher.map("/muse/elements/experimental/mellow", print)
+#   dispatcher.map("/muse/elements/horseshoe", print)
+#   dispatcher.map("/muse/elements/is_good", print)
+  dispatcher.map("/muse/eeg", print)
+  
+
+  server = osc_server.ThreadingOSCUDPServer(
+      (args.ip, args.port), dispatcher)
+  print("Serving on {}".format(server.server_address))
+  server.serve_forever()
