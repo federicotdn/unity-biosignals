ccd6e504dea26b6f48267a789ae8b23e4a5ac628
Fede <federicotedin@gmail.com>
2016-12-18 22:53:23 -0300

    added emg reading with arduino

---
+++ Scripts/Core/pf-core/EMG/EMGPacket.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace pfcore {
+    public class EMGPacket {
+        // Data packet received from Olimexino328
+        private const int DATA_LEN = 6;
+        private const int DATA_OFFSET = 4;
+
+        public const int PACKET_SIZE = 17;
+        public const byte SYNC0_BYTE = 0xA5;
+        public const byte SYNC1_BYTE = 0x5A;
+        public const byte VERSION_BYTE = 2;
+
+        public byte sync0;
+        public byte sync1;
+        public byte version;
+        public byte count;
+        public UInt16[] data;
+        public byte switches;
+        // 17 bytes total
+
+        public EMGPacket() {
+            data = new UInt16[DATA_LEN];
+        }
+
+        public void Unpack(byte[] buf) {
+            sync0 = buf[0];
+            sync1 = buf[1];
+            version = buf[2];
+            count = buf[3];
+
+            int dataIdx = 0;
+            for (int i = DATA_OFFSET; i < DATA_OFFSET + (DATA_LEN * 2); i += 2) {
+                byte upper = buf[i];
+                byte lower = buf[i + 1];
+
+                UInt16 val = (UInt16)(upper << 8);
+                val += lower;
+                data[dataIdx++] = val;
+            }
+
+            switches = buf[16];
+        }
+    }
+}
---
+++ Scripts/Core/pf-core/EMG/EMGReader.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace pfcore {
+    public class EMGReader {
+        // Values for Olimexino328 Arduino sketch
+        private const int baudRate = 57600;
+        private const Parity parity = Parity.None;
+        private const int dataBits = 8;
+        private const StopBits stopBits = StopBits.One;
+        private const int readTimeout = 500;
+
+        private SerialPort serialPort;
+
+        public EMGReader(string portName) {
+            serialPort = new SerialPort(portName, baudRate, parity, dataBits, stopBits);
+            serialPort.ReadTimeout = SerialPort.InfiniteTimeout;
+        }
+
+        public void Start() {
+            serialPort.Open();
+
+            byte[] buffer = new byte[EMGPacket.PACKET_SIZE];
+            EMGPacket packet = new EMGPacket();
+
+            while (true) {
+                bool readOk = true;
+                for (int i = 0; i < buffer.Length && readOk; i++) {
+                    int val = serialPort.ReadByte();
+                    if (val == -1) {
+                        throw new Exception("End of stream.");
+                    }
+
+                    byte readByte = (byte)val;
+
+                    if ((i == 0 && readByte != EMGPacket.SYNC0_BYTE) ||
+                        (i == 1 && readByte != EMGPacket.SYNC1_BYTE) ||
+                        (i == 2 && readByte != EMGPacket.VERSION_BYTE)) {
+
+                        readOk = false;
+                    }
+
+                    buffer[i] = readByte;
+                }
+
+                if (readOk) {
+                    packet.Unpack(buffer);
+
+                    Console.WriteLine("sync0: " + packet.sync0);
+                    Console.WriteLine("sync1: " + packet.sync1);
+                    Console.WriteLine("count: " + packet.count);
+                    Console.WriteLine("version: " + packet.version);
+                    Console.WriteLine("switches: " + packet.switches);
+                    Console.Write("data: \n[");
+                    foreach (byte b in packet.data) {
+                        Console.Write(b + ", ");
+                    }
+                    Console.WriteLine("]");
+                    Console.WriteLine("-----");
+                } else {
+                    Console.WriteLine("skipped");
+                    Console.WriteLine("------");
+                }
+            }
+        }
+    }
+}
