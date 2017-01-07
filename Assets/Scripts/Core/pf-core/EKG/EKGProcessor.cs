689f179bf25a18ea4d900ca63177209a7753c8fb
Fede <federicotedin@gmail.com>
2017-01-06 21:58:52 -0300

    starting with emg tests on unity

--- Assets/Scripts/Core/pf-core/EKG/EKGProcessor.cs
+++ Assets/Scripts/Core/pf-core/EKG/EKGProcessor.cs
@@ -2,9 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace pfcore {
     class EKGProcessor {
+        private EKGReader reader;
+        private readonly long baseTime;
+
+        private List<EKGPacket> readings = new List<EKGPacket>();
+
+        public EKGProcessor(EKGReader reader) {
+            this.reader = reader;
+            baseTime = DateTime.Now.Ticks;
+        }
+
+        public void Start() {
+            Thread thread = new Thread(new ThreadStart(reader.Start));
+            thread.Start();
+        }
+
+        public void Update() {
+            ConcurrentQueue<EKGPacket> queue = reader.PacketQueue;
+            EKGPacket packet;
+            while (queue.TryDequeue(out packet)) {
+                readings.Add(packet);
+            }
+
+            // Do stuff
+
+            while (queue.TryDequeue(out packet)) {
+                /* Discard packets */
+            }
+        }
+
     }
 }
