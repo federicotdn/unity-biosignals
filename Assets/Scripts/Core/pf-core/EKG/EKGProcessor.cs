524d31059b4c846387a92c9457bcc358117f0bea
Javier Fraire <javierfraire92@gmail.com>
2017-01-09 11:48:44 -0300

    Added EKG bpm

--- Assets/Scripts/Core/pf-core/EKG/EKGProcessor.cs
+++ Assets/Scripts/Core/pf-core/EKG/EKGProcessor.cs
@@ -5,36 +5,72 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace pfcore {
-    class EKGProcessor {
-        private EKGReader reader;
-        private readonly long baseTime;
+namespace pfcore
+{
+	class EKGProcessor
+	{
+		private EKGReader reader;
+		private readonly long baseTime;
 
-        private List<EKGPacket> readings = new List<EKGPacket>();
+		private Queue<long> peaks = new Queue<long>();
 
-        public EKGProcessor(EKGReader reader) {
-            this.reader = reader;
-            baseTime = DateTime.Now.Ticks;
-        }
+		private bool previousPeak;
 
-        public void Start() {
-            Thread thread = new Thread(new ThreadStart(reader.Start));
-            thread.Start();
-        }
 
-        public void Update() {
-            ConcurrentQueue<EKGPacket> queue = reader.PacketQueue;
-            EKGPacket packet;
-            while (queue.TryDequeue(out packet)) {
-                readings.Add(packet);
-            }
+		private const long windowSize = 10;
 
-            // Do stuff
+		public EKGProcessor(EKGReader reader)
+		{
+			this.reader = reader;
+			baseTime = DateTime.Now.Ticks;
+		}
 
-            while (queue.TryDequeue(out packet)) {
-                /* Discard packets */
-            }
-        }
+		public void Start()
+		{
+			Thread thread = new Thread(new ThreadStart(reader.Start));
+			thread.Start();
+		}
 
-    }
+		public void Update()
+		{
+			ConcurrentQueue<EKGPacket> queue = reader.PacketQueue;
+			EKGPacket packet;
+			while (queue.TryDequeue(out packet))
+			{
+				if (!previousPeak)
+				{
+					previousPeak = true;
+					peaks.Enqueue(packet.timeStamp);
+				}
+				else
+				{
+					previousPeak = false;
+				}
+
+				// Discard old packages
+				while (peaks.Count > 0 && TimeSpan.FromTicks(DateTime.Now.Ticks - peaks.Peek()).TotalSeconds > windowSize)
+				{
+					peaks.Dequeue();
+				}
+			}
+
+			// Do stuff
+
+			while (queue.TryDequeue(out packet))
+			{
+				/* Discard packets */
+			}
+		}
+
+		public int getBPM()
+		{
+			if (peaks.Count == 0)
+			{
+				return 0;
+			}
+
+			return (int)((60 * peaks.Count) / TimeSpan.FromTicks(DateTime.Now.Ticks - peaks.Peek()).TotalSeconds);
+		}
+
+	}
 }
