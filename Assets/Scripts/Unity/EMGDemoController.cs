e0253cce6fed60e86fd8f1ba08cdba19c35dc496
Fede <ftedinur@itba.edu.ar>
2017-02-01 22:28:51 -0300

    moved unity scenes, created EMGDemo scene

---
+++ Assets/Scripts/Unity/EMGDemoController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class EMGDemoController : MonoBehaviour {
+
+    public GameObject cube;
+
+	// Use this for initialization
+	void Start () {
+        EMGManager.Instance.Setup();
+        EMGManager.Instance.StartReading();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
