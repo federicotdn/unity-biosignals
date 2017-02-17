151578fc3d9164ca80d22ffe34cf7538b3508f4a
Javier Fraire <javierfraire92@gmail.com>
2017-02-16 21:38:59 -0300

    Added main menu

---
+++ Assets/Scripts/Unity/Managers/MenuManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviorSingleton<MenuManager> {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void LoadScene(int scene) {
+		SceneManager.LoadScene (scene);
+	}
+}
